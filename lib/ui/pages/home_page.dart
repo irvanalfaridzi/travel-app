2b612bb87492cf60d0c903bfd4cde88ee65705b0
Irvan Alfaridzi <irvanalfaridzi@gmail.com>
2023-01-27 15:01:56 +0700

    added home screen and google font

---
+++ lib/ui/pages/home_page.dart
@@ -0,0 +1,189 @@
+part of 'pages.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColor.whiteColor,
+      bottomNavigationBar: Container(
+        width: MediaQuery.of(context).size.width,
+        height: 75,
+        decoration: const BoxDecoration(
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(20),
+            topRight: Radius.circular(20),
+          ),
+          boxShadow: [
+            BoxShadow(
+              blurRadius: 10,
+              spreadRadius: 0,
+              color: AppColor.shadowColor,
+              offset: Offset(0, -4),
+            )
+          ],
+          color: AppColor.whiteColor,
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            NavBarButtonItem(
+              iconButton: Icons.home_rounded,
+              textButton: "Home",
+              isSelected: true,
+              onTap: () {},
+            ),
+            NavBarButtonItem(
+              iconButton: Icons.favorite_rounded,
+              textButton: "Saved",
+              isSelected: false,
+              onTap: () {},
+            ),
+            NavBarButtonItem(
+              iconButton: Icons.featured_play_list_rounded,
+              textButton: "Orders",
+              isSelected: false,
+              onTap: () {},
+            ),
+          ],
+        ),
+      ),
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(height: 58),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 40),
+              child: Text(
+                "Find Your",
+                style: GoogleFonts.poppins(
+                  color: AppColor.blackColor,
+                  fontSize: 32,
+                  fontWeight: FontWeight.w300,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 40),
+              child: Text(
+                "great vacation",
+                style: GoogleFonts.poppins(
+                  color: AppColor.darkBlueColor,
+                  fontSize: 32,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ),
+            const SizedBox(height: 45),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              margin: const EdgeInsets.symmetric(horizontal: 40),
+              decoration: BoxDecoration(
+                color: AppColor.softBlueColor,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: TextField(
+                decoration: InputDecoration(
+                  contentPadding: const EdgeInsets.only(top: 14),
+                  border: InputBorder.none,
+                  prefixIcon: const Icon(
+                    Icons.search,
+                    color: AppColor.blueColor,
+                  ),
+                  hintText: "Let's find here",
+                  hintStyle: GoogleFonts.poppins(
+                    color: AppColor.lightBlueColor,
+                    fontWeight: FontWeight.w500,
+                    fontSize: 12,
+                  ),
+                ),
+                cursorColor: AppColor.darkBlueColor,
+              ),
+            ),
+            const SizedBox(height: 23),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 40),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Popular tips",
+                    style: GoogleFonts.poppins(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w600,
+                      color: AppColor.blackColor,
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      "See all",
+                      style: GoogleFonts.poppins(
+                        fontSize: 13,
+                        fontWeight: FontWeight.w500,
+                        color: AppColor.darkGreyColor,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 4),
+            SizedBox(
+              width: MediaQuery.of(context).size.width,
+              height: 220,
+              child: Center(
+                child: ListView.builder(
+                  itemCount: 5,
+                  scrollDirection: Axis.horizontal,
+                  padding: const EdgeInsets.only(left: 40, top: 13, bottom: 13),
+                  itemBuilder: (context, index) => Padding(
+                    padding: const EdgeInsets.only(right: 20),
+                    child: InkWell(
+                      onTap: () {
+                        // TODO: add navigation to detail vacation item
+                      },
+                      child: Container(
+                        width: 145,
+                        height: 194,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: AppColor.whiteColor,
+                          boxShadow: const [
+                            BoxShadow(
+                              blurRadius: 10,
+                              spreadRadius: 0,
+                              color: AppColor.shadowColor,
+                              offset: Offset(0, 1),
+                            )
+                          ],
+                        ),
+                        child: Column(
+                          children: [
+                            Container(
+                              height: 119,
+                              decoration: const BoxDecoration(
+                                borderRadius: BorderRadius.only(
+                                  topLeft: Radius.circular(10),
+                                  topRight: Radius.circular(10),
+                                ),
+                                color: AppColor.darkBlueColor,
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/ui/widgets/navbar_button_item.dart
@@ -0,0 +1,44 @@
+part of 'widgets.dart';
+
+class NavBarButtonItem extends StatelessWidget {
+  final String textButton;
+  final IconData iconButton;
+  final bool isSelected;
+  final Function onTap;
+  const NavBarButtonItem({
+    super.key,
+    required this.iconButton,
+    required this.textButton,
+    required this.isSelected,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final buttonColor =
+        isSelected ? AppColor.darkBlueColor : AppColor.greyColor;
+    return TextButton(
+      onPressed: () {
+        onTap();
+      },
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(
+            iconButton,
+            color: buttonColor,
+          ),
+          const SizedBox(height: 8),
+          Text(
+            textButton,
+            style: GoogleFonts.poppins(
+              color: buttonColor,
+              fontSize: 10,
+              fontWeight: FontWeight.w400,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
