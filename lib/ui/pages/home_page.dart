39522617d768f0c9ab0be434af30658c6c1b2a76
Irvan Alfaridzi <irvanalfaridzi@gmail.com>
2023-01-27 15:02:56 +0700

    added singlechildscrollview in home page

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -51,137 +51,140 @@ class HomePage extends StatelessWidget {
         ),
       ),
       body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const SizedBox(height: 58),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 40),
-              child: Text(
-                "Find Your",
-                style: GoogleFonts.poppins(
-                  color: AppColor.blackColor,
-                  fontSize: 32,
-                  fontWeight: FontWeight.w300,
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 40),
-              child: Text(
-                "great vacation",
-                style: GoogleFonts.poppins(
-                  color: AppColor.darkBlueColor,
-                  fontSize: 32,
-                  fontWeight: FontWeight.w500,
-                ),
-              ),
-            ),
-            const SizedBox(height: 45),
-            Container(
-              width: MediaQuery.of(context).size.width,
-              margin: const EdgeInsets.symmetric(horizontal: 40),
-              decoration: BoxDecoration(
-                color: AppColor.softBlueColor,
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: TextField(
-                decoration: InputDecoration(
-                  contentPadding: const EdgeInsets.only(top: 14),
-                  border: InputBorder.none,
-                  prefixIcon: const Icon(
-                    Icons.search,
-                    color: AppColor.blueColor,
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(height: 58),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 40),
+                child: Text(
+                  "Find Your",
+                  style: GoogleFonts.poppins(
+                    color: AppColor.blackColor,
+                    fontSize: 32,
+                    fontWeight: FontWeight.w300,
                   ),
-                  hintText: "Let's find here",
-                  hintStyle: GoogleFonts.poppins(
-                    color: AppColor.lightBlueColor,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 40),
+                child: Text(
+                  "great vacation",
+                  style: GoogleFonts.poppins(
+                    color: AppColor.darkBlueColor,
+                    fontSize: 32,
                     fontWeight: FontWeight.w500,
-                    fontSize: 12,
                   ),
                 ),
-                cursorColor: AppColor.darkBlueColor,
               ),
-            ),
-            const SizedBox(height: 23),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 40),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    "Popular tips",
-                    style: GoogleFonts.poppins(
-                      fontSize: 16,
-                      fontWeight: FontWeight.w600,
-                      color: AppColor.blackColor,
+              const SizedBox(height: 45),
+              Container(
+                width: MediaQuery.of(context).size.width,
+                margin: const EdgeInsets.symmetric(horizontal: 40),
+                decoration: BoxDecoration(
+                  color: AppColor.softBlueColor,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: TextField(
+                  decoration: InputDecoration(
+                    contentPadding: const EdgeInsets.only(top: 14),
+                    border: InputBorder.none,
+                    prefixIcon: const Icon(
+                      Icons.search,
+                      color: AppColor.blueColor,
+                    ),
+                    hintText: "Let's find here",
+                    hintStyle: GoogleFonts.poppins(
+                      color: AppColor.lightBlueColor,
+                      fontWeight: FontWeight.w500,
+                      fontSize: 12,
                     ),
                   ),
-                  TextButton(
-                    onPressed: () {},
-                    child: Text(
-                      "See all",
+                  cursorColor: AppColor.darkBlueColor,
+                ),
+              ),
+              const SizedBox(height: 23),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 40),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "Popular tips",
                       style: GoogleFonts.poppins(
-                        fontSize: 13,
-                        fontWeight: FontWeight.w500,
-                        color: AppColor.darkGreyColor,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600,
+                        color: AppColor.blackColor,
                       ),
                     ),
-                  ),
-                ],
-              ),
-            ),
-            const SizedBox(height: 4),
-            SizedBox(
-              width: MediaQuery.of(context).size.width,
-              height: 220,
-              child: Center(
-                child: ListView.builder(
-                  itemCount: 5,
-                  scrollDirection: Axis.horizontal,
-                  padding: const EdgeInsets.only(left: 40, top: 13, bottom: 13),
-                  itemBuilder: (context, index) => Padding(
-                    padding: const EdgeInsets.only(right: 20),
-                    child: InkWell(
-                      onTap: () {
-                        // TODO: add navigation to detail vacation item
-                      },
-                      child: Container(
-                        width: 145,
-                        height: 194,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          color: AppColor.whiteColor,
-                          boxShadow: const [
-                            BoxShadow(
-                              blurRadius: 10,
-                              spreadRadius: 0,
-                              color: AppColor.shadowColor,
-                              offset: Offset(0, 1),
-                            )
-                          ],
+                    TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        "See all",
+                        style: GoogleFonts.poppins(
+                          fontSize: 13,
+                          fontWeight: FontWeight.w500,
+                          color: AppColor.darkGreyColor,
                         ),
-                        child: Column(
-                          children: [
-                            Container(
-                              height: 119,
-                              decoration: const BoxDecoration(
-                                borderRadius: BorderRadius.only(
-                                  topLeft: Radius.circular(10),
-                                  topRight: Radius.circular(10),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 4),
+              SizedBox(
+                width: MediaQuery.of(context).size.width,
+                height: 220,
+                child: Center(
+                  child: ListView.builder(
+                    itemCount: 5,
+                    scrollDirection: Axis.horizontal,
+                    padding:
+                        const EdgeInsets.only(left: 40, top: 13, bottom: 13),
+                    itemBuilder: (context, index) => Padding(
+                      padding: const EdgeInsets.only(right: 20),
+                      child: InkWell(
+                        onTap: () {
+                          // TODO: add navigation to detail vacation item
+                        },
+                        child: Container(
+                          width: 145,
+                          height: 194,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            color: AppColor.whiteColor,
+                            boxShadow: const [
+                              BoxShadow(
+                                blurRadius: 10,
+                                spreadRadius: 0,
+                                color: AppColor.shadowColor,
+                                offset: Offset(0, 1),
+                              )
+                            ],
+                          ),
+                          child: Column(
+                            children: [
+                              Container(
+                                height: 119,
+                                decoration: const BoxDecoration(
+                                  borderRadius: BorderRadius.only(
+                                    topLeft: Radius.circular(10),
+                                    topRight: Radius.circular(10),
+                                  ),
+                                  color: AppColor.darkBlueColor,
                                 ),
-                                color: AppColor.darkBlueColor,
-                              ),
-                            )
-                          ],
+                              )
+                            ],
+                          ),
                         ),
                       ),
                     ),
                   ),
                 ),
-              ),
-            )
-          ],
+              )
+            ],
+          ),
         ),
       ),
     );
