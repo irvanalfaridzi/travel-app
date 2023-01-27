e4289d1e5c7a33d03c149425fd8624a459767855
Irvan Alfaridzi <irvanalfaridzi@gmail.com>
2023-01-27 15:01:28 +0700

    added app color

---
+++ lib/util/color.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static const Color blackColor = Color(0xff000000);
+  static const Color whiteColor = Color(0xffffffff);
+  static const Color darkBlueColor = Color(0xff06152A);
+  static const Color blueColor = Color(0xff005DC6);
+  static const Color lightBlueColor = Color(0xff6394D8);
+  static const Color softBlueColor = Color(0xffEAF2FD);
+  static const Color yellowColor = Color(0xffFBD02F);
+  static const Color darkGreyColor = Color(0xff555E6B);
+  static const Color greyColor = Color(0xffA7A7A7);
+  static const Color purpleColor = Color(0xffA78EF6);
+  static const Color shadowColor = Color(0xffD6D6D6);
+}
